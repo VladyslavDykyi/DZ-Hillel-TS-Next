4d964cd671e61278a5a9800f3366c936d5f382bc
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-10-27 23:59:36 +0200

    dz 2

---
+++ src/app.js
@@ -0,0 +1,10 @@
+var product = {
+    name: 'Молоко',
+    price: 100,
+    availability: true,
+};
+var alertProduct = function (value) {
+    var availability = value.availability ? 'Так' : 'Ні';
+    return "\u0422\u043E\u0432\u0430\u0440: ".concat(value.name, ", \u0426\u0456\u043D\u0430: ").concat(value.price, " \u0433\u0440\u043D., \u0412 \u043D\u0430\u044F\u0432\u043D\u043E\u0441\u0442\u0456: ").concat(value.availability, ".");
+};
+console.log(alertProduct(product));
---
+++ src/app.ts
@@ -0,0 +1,16 @@
+interface Product {
+    name: string,
+    price: number,
+    availability: boolean,
+}
+
+const product = {
+    name: 'Молоко',
+    price: 100,
+    availability: true,
+};
+const alertProduct = (value: Product): string => {
+    const availability = value.availability ? 'Так' : 'Ні';
+    return `Товар: ${value.name}, Ціна: ${value.price} грн., В наявності: ${value.availability}.`
+}
+console.log(alertProduct(product));
