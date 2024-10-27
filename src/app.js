8a4e5460418de4f6f5e816ba90beaf9da0e98edf
VladyslavDykyi <vladislavdykyi33@gmail.com>
2024-10-28 00:00:08 +0200

    dz 2

--- src/app.js
+++ src/app.js
@@ -5,6 +5,6 @@ var product = {
 };
 var alertProduct = function (value) {
     var availability = value.availability ? 'Так' : 'Ні';
-    return "\u0422\u043E\u0432\u0430\u0440: ".concat(value.name, ", \u0426\u0456\u043D\u0430: ").concat(value.price, " \u0433\u0440\u043D., \u0412 \u043D\u0430\u044F\u0432\u043D\u043E\u0441\u0442\u0456: ").concat(value.availability, ".");
+    return "\u0422\u043E\u0432\u0430\u0440: ".concat(value.name, ", \u0426\u0456\u043D\u0430: ").concat(value.price, " \u0433\u0440\u043D., \u0412 \u043D\u0430\u044F\u0432\u043D\u043E\u0441\u0442\u0456: ").concat(availability, ".");
 };
 console.log(alertProduct(product));
